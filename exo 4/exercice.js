00c013cc1cac88a4c5d75ef1e48a33aaa3501b5c
Sylvie-B <unsolo@hotmail.fr>
2020-12-04 11:49:25 +0100

    planets

--- exo 4/exercice.js
+++ exo 4/exercice.js
@@ -1,11 +1,21 @@
+let planet = document.createElement("div");
 
+planet.className = "planet";
+planet.style.backgroundColor = "blue";
 
+document.body.appendChild(planet);
 
+let moon = document.createElement("div");
+moon.className ="moon";
+moon.style.backgroundColor = "yellow";
 
+document.body.appendChild(moon);
 
+let mars = document.createElement("div");
+mars.className ="planet";
 
+mars.style.backgroundColor = "red";
+mars.style.left = "80px";
+mars.style.top = "50px";
 
-
-
-
-
+document.body.appendChild(mars);
