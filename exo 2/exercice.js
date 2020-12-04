4e57fbc46da374e49c915708264e189630bd40b0
Sylvie-B <unsolo@hotmail.fr>
2020-12-04 11:20:39 +0100

    exo 73 exo 2

--- exo 2/exercice.js
+++ exo 2/exercice.js
@@ -1,9 +1,12 @@
+
 let colorEl = document.getElementById("trendy-color");
 let handEl  = document.getElementById("trendy-handwriting");
 let frameEl = document.getElementById("trendy-frame");
 
+colorEl.innerHTML = "bleu";
+colorEl.style.color = "blue";
 
+handEl.innerHTML = "Legolas";
+handEl.style.fontFamily = "cursive";
 
-
-
-
+frameEl.style.border = "double 4px red";
