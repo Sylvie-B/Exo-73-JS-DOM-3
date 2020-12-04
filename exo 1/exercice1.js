212150ce87de9c5df0b889792f8fe809937c9065
Sylvie-B <unsolo@hotmail.fr>
2020-12-04 10:54:14 +0100

    taille et infobull

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,12 +1,7 @@
 let avatarEls = document.getElementsByTagName("img");
 
-
-
-
-
-
-
-
-
-
+for(let a = 0; a < avatarEls.length; a++){
+    avatarEls[a].style.height = "90px";
+    avatarEls[a].title = avatarEls[a].alt;
+}
 
